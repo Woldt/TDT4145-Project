b0feb9ddb39a704dce8ff74a34b5fa6588462f47
Sigve Andre Evensen Skaugvoll <saskaugv@stud.ntnu.no>
2016-03-07 23:59:05 +0100

    added workoutTitle validatation and more UI-model logic

--- TDT4145/src/application/Workout.java
+++ TDT4145/src/application/Workout.java
@@ -5,6 +5,8 @@ import java.beans.PropertyChangeSupport;
 
 public class Workout {
 	
+	private String workoutTitle;
+	
 	private PropertyChangeSupport pcs = new PropertyChangeSupport(this);
 	public static String TITLE_PROPERTY = "title";
 
@@ -15,6 +17,27 @@ public class Workout {
 
 	public void removePropertyChangeListener(PropertyChangeListener listener) {
 		pcs.removePropertyChangeListener(listener);
+	}
+
+	public void setWorkoutTitle(String workoutTitle) {
+		String oldValue = "";
+		if(! workoutTitle.matches("[a-zA-z]")){
+			throw new IllegalStateException("Not valid workoutTitle");
+		}
+		else if(workoutTitle.isEmpty()){
+			oldValue = this.workoutTitle;
+			this.workoutTitle = "";
+			pcs.firePropertyChange(TITLE_PROPERTY, oldValue, workoutTitle);
+		}
+		else{
+			oldValue = this.workoutTitle;
+			this.workoutTitle = workoutTitle;
+			pcs.firePropertyChange(TITLE_PROPERTY, oldValue, workoutTitle);
+		}
+	}
+
+	public String getWorkoutTitle() {
+		return this.workoutTitle;
 	}
 
 }
