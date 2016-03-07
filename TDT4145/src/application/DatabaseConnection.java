4aec292f52e83f4eabadc19fe5bcddfeca904fc4
Sigve Andre Evensen Skaugvoll <saskaugv@stud.ntnu.no>
2016-03-07 18:30:27 +0100

    cleaned up code. no Main function, only methods.. Not tested!

--- TDT4145/src/application/DatabaseConnection.java
+++ TDT4145/src/application/DatabaseConnection.java
@@ -12,9 +12,12 @@ public class DatabaseConnection {
 	static final String USER = "username";
 	static final String PASS = "password";
 
-	public static void main(String[] args) {
-		Connection conn = null;
-		Statement stmt = null;
+	private Connection conn = null;
+	private Statement stmt = null;
+	
+	private ResultSet rs;
+
+	private void establishConnection(){	
 		try{
 			//STEP 2: Register JDBC driver
 			Class.forName("com.mysql.jdbc.Driver");
@@ -22,15 +25,33 @@ public class DatabaseConnection {
 			//STEP 3: Open a connection
 			System.out.println("Connecting to database...");
 			conn = DriverManager.getConnection(DB_URL,USER,PASS);
-
-			//STEP 4: Execute a query
-			System.out.println("Creating statement...");
+		}
+		catch(SQLException se){
+			//Handle errors for JDBC
+			se.printStackTrace();
+		}
+		catch(Exception e){
+			//Handle errors for Class.forName
+			e.printStackTrace();
+		}
+	}
+	
+	private void executeQuery(String query){
+		//STEP 4: Execute a query
+		System.out.println("Creating statement...");
+		try {
 			stmt = conn.createStatement();
-			String sql;
-			sql = "SELECT * FROM ";
-			ResultSet rs = stmt.executeQuery(sql);
-
-			//STEP 5: Extract data from result set
+			rs = stmt.executeQuery(query);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+	
+	private void resultToString(){
+		//STEP 5: Extract data from result set
+		try {
 			while(rs.next()){
 				//Retrieve by column name
 				int id  = rs.getInt("id");
@@ -39,36 +60,29 @@ public class DatabaseConnection {
 				String last = rs.getString("last");
 
 				//Display values
-				System.out.print("ID: " + id);
-				System.out.print(", Age: " + age);
-				System.out.print(", First: " + first);
-				System.out.println(", Last: " + last);
+
 			}
-			//STEP 6: Clean-up environment
-			rs.close();
-			stmt.close();
-			conn.close();
-		}catch(SQLException se){
-			//Handle errors for JDBC
-			se.printStackTrace();
-		}catch(Exception e){
-			//Handle errors for Class.forName
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}finally{
-			//finally block used to close resources
-			try{
-				if(stmt!=null)
-					stmt.close();
-			}catch(SQLException se2){
-			}// nothing we can do
-			try{
-				if(conn!=null)
-					conn.close();
-			}catch(SQLException se){
-				se.printStackTrace();
-			}//end finally try
-		}//end try
-		System.out.println("Goodbye!");
-	}//end main
+		}
+
+	}
+	
+	private void terminateConnection(){
+		//STEP 6: Clean-up environment
+		try{
+			if(stmt!=null)
+				stmt.close();
+		}catch(SQLException se2){
+		}// nothing we can do
+		try{
+			if(conn!=null)
+				conn.close();
+		}catch(SQLException se){
+			se.printStackTrace();
+		}
+	}
+	
 
 }
