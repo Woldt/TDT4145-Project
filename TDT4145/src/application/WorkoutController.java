59a2bda43ae6ef41bbb87c3e40a1282e1e7746c2
Sigve Andre Evensen Skaugvoll <saskaugv@stud.ntnu.no>
2016-03-07 23:20:26 +0100

    Added initialization of comboboxes, and UI-modell Logic

--- TDT4145/src/application/WorkoutController.java
+++ TDT4145/src/application/WorkoutController.java
@@ -1,5 +1,8 @@
 package application;
 
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
+
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
@@ -7,21 +10,77 @@ import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-public class WorkoutController {
+public class WorkoutController implements PropertyChangeListener {
+
+	private Workout workout;
+
 	@FXML private TextField workoutID;
 	@FXML private DatePicker dateField;
 	@FXML private ComboBox<String> typeField; 
-	@FXML private ComboBox<Integer> minuteField; 
-	@FXML private ComboBox<Integer> hourField; 
-	@FXML private ComboBox<Integer> durationField; 
+	@FXML private ComboBox<String> minuteField; 
+	@FXML private ComboBox<String> hourField; 
+	@FXML private ComboBox<String> durationField; 
 	@FXML private ComboBox<String> feelingField; 
 	@FXML private ComboBox<String> accomplishmentField; 
 	@FXML private TextArea personalNoteField; 
 	@FXML private Button createBtn;
 	@FXML private Button cancelBtn;
 	@FXML private Button resetBtn;
+
+	//	MAKING THE UI-model: 
+	private void setModel(Workout workout){
+		if(this.workout != null){
+			this.workout.removePropertyChangeListener(this);
+		}
+		this.workout = workout;
+
+		if(this.workout != null){
+			this.workout.addPropertyChangeListener(this);
+		}
+	}
+
+
+	@Override
+	public void propertyChange(PropertyChangeEvent event) {
+		if(event.getSource() != this.workout){
+			return;
+		}
+		String propertyName = event.getPropertyName();
+
+	}
+
+
+	// Start of CONTROLLER Code:
+	public void initialize(){
+		setModel(new Workout());
+		initFrom();
+		initDuration();
+		initWorkoutType();
+
+
+	}
+
+	private void initFrom() {
+		for(int i = 7; i < 24; i++){
+			if(i < 10 ? hourField.getItems().add("0"+i) : hourField.getItems().add(String.valueOf(i)));
+		}
+		for(int j = 0; j < 60; j += 15){
+			if(j < 10 ? minuteField.getItems().add("0"+ j) : minuteField.getItems().add(String.valueOf(j)));
+		}
+	}
+
+
+	private void initDuration() {
+		for(int d = 0; d <= 300; d += 30){
+			if(d < 10 ? durationField.getItems().add("0"+d+" min") : durationField.getItems().add(String.valueOf(d+" min")));
+		}
+	}	
 	
 	
-	
-	
+	private void initWorkoutType() {
+		typeField.getItems().addAll("Uteaktivitet","Inneaktivitet");
+	}
+
+
+
 }
