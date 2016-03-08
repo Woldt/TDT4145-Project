bfb9d8d3e5f00787bfd162b93897d06706c4257b
Sigve Andre Evensen Skaugvoll <saskaugv@stud.ntnu.no>
2016-03-08 15:06:16 +0100

    Added scene handling

--- TDT4145/src/application/Main.java
+++ TDT4145/src/application/Main.java
@@ -8,8 +8,11 @@ import javafx.stage.Stage;
 
 public class Main extends Application{
 
+	public static Stage primaryStage;
+	
 	@Override
 	public void start(Stage primaryStage) throws Exception {
+		Main.primaryStage = primaryStage;
 		FXMLLoader fxmlLoader = new FXMLLoader();
 		Parent root = (Parent) fxmlLoader.load(this.getClass().getResourceAsStream("DiaryGUI.fxml"));
 		primaryStage.setScene(new Scene(root));
