104729797954cf5a13b70396db6d959a8a1eeae3
Sigve Andre Evensen Skaugvoll <saskaugv@stud.ntnu.no>
2016-03-08 12:24:59 +0100

    added fxml fields, initialization method with helping initMethods

---
+++ TDT4145/src/application/ExcercisesController.java
@@ -0,0 +1,58 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+public class ExcercisesController {
+	
+	@FXML Label workoutTitle;
+	@FXML Label	repsTimesSets;
+	@FXML TextArea descriptionField;
+	@FXML TextField exerciseField;
+	@FXML TextField weightField;
+	@FXML TextField goalField;
+	@FXML TextField repsField;
+	@FXML TextField setsField;
+	@FXML TextField lenghtField;
+	@FXML ComboBox<String> groupField;
+	@FXML ComboBox<String> specOneField;
+	@FXML ComboBox<String> specTwoField;
+	@FXML ComboBox<String> typeField;
+	@FXML Button moreBtn;
+	@FXML Button addBtn;
+	
+	private void initialization(){
+//		initWorkoutName();
+		initType();
+		initTypeSpecifics();
+		
+	}
+
+	private void initType() {
+		typeField.getItems().addAll("Strength","Endurance");
+	}
+	
+	//Remeber to use type from workout!!!!
+	private void initTypeSpecifics() {
+		if(typeField.getValue() == null){
+			//Do some view-logic...
+		}
+		else if(typeField.getValue().equals("Outside")){
+			specOneField.setPromptText("Temp");
+			specTwoField.setPromptText("Weather");
+		}
+		else if(typeField.getValue().equals("Inside")){
+			specOneField.setPromptText("AC state");
+			specTwoField.setPromptText("Weather");
+		}
+		
+	}
+	
+	
+	
+
+}
