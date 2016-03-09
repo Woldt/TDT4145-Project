b6492199921b1117ea7f5d5701a538cb5139438c
Sigve Andre Evensen Skaugvoll <saskaugv@stud.ntnu.no>
2016-03-09 13:45:02 +0100

    added more endurance exercise validation

--- TDT4145/src/application/ExcercisesController.java
+++ TDT4145/src/application/ExcercisesController.java
@@ -14,10 +14,10 @@ import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
 public class ExcercisesController  implements PropertyChangeListener {
-	
+
 	private Strength strengthExercise;
 	private Endurance enduranceExercise;
-	
+
 	@FXML Label workoutTitle;
 	@FXML Label	repsTimesSets;
 	@FXML TextArea descriptionField;
@@ -34,7 +34,7 @@ public class ExcercisesController  implements PropertyChangeListener {
 	@FXML Button moreBtn;
 	@FXML Button addBtn;
 
-// Making the UI-model:
+	// Making the UI-model:
 	private void setStrenghtModel(Strength exerciseModel){
 		if(this.strengthExercise != null){
 			this.strengthExercise.removePropertyChangeListener(this);
@@ -43,9 +43,9 @@ public class ExcercisesController  implements PropertyChangeListener {
 		if(this.strengthExercise != null) {
 			this.strengthExercise.addPropertyChangeListener(this);
 		}
-	
+
 	}
-	
+
 	private void setEnduranceModel(Endurance exerciseModel){
 		if(this.enduranceExercise != null){
 			this.enduranceExercise.removePropertyChangeListener(this);
@@ -54,22 +54,22 @@ public class ExcercisesController  implements PropertyChangeListener {
 		if(this.enduranceExercise != null) {
 			this.enduranceExercise.addPropertyChangeListener(this);
 		}
-	
+
 	}
-	
+
 	@Override
 	public void propertyChange(PropertyChangeEvent event) {
 		if(event.getSource() != this.strengthExercise || event.getSource() != this.enduranceExercise){
 			return;
 		}
 		String propertyName = event.getPropertyName();
-		
-		
-		
+
+
+
 	}	
-	
-	
-// Initialization:
+
+
+	// Initialization:
 	public void initialize(){
 		setWorkoutTitle();
 		initType();
@@ -81,11 +81,11 @@ public class ExcercisesController  implements PropertyChangeListener {
 		groupField.getItems().addAll("Neck","Shoulders","Checst","Shield","Triceps","Biceps","Pecs","Abs","Thighs","Calvs","Core");
 	}
 
-	
+
 	private void initType() {
 		typeField.getItems().addAll("Strength","Endurance");
 	}
-	
+
 	//Remeber to use type from workout!!!!
 	private void initTypeSpecifics() {
 		String workoutType = Workout.getWorkoutType();
@@ -110,202 +110,209 @@ public class ExcercisesController  implements PropertyChangeListener {
 				if(s < 10 ? specTwoField.getItems().add("0"+s) : s == 11 ? specTwoField.getItems().add(s+"+") : specTwoField.getItems().add(String.valueOf(s)));
 			}
 		}
-		
+
 	}
 
 	private void setWorkoutTitle(){
 		workoutTitle.setText(Workout.getWorkoutTitle());
-		
-	}
-	
-//Scene handler: All validation, model update and view update must happend here. First thing we must do, is set the model based on exercise type!!
-		@FXML
-		private void addBtnClicked(){
-			/* Exercise attributes that cannot be Null:
-			 * ØvelsesID - int - auto_increment - PK- Unique.
-			 * Navn - varchar(45)
-			 * Beskrivelse - varchar(45)
-			 * 
-			 * Strength-tabell:
-			 * ØvelsesID - int
-			 * Belastning - int
-			 * Antall repetisjoner - int
-			 * Antall sett - int
-			 * 
-			 * Utholdenhet-tabell
-			 * ØvelsesID - int
-			 * Lengde km - int
-			 * Minutter - int
-			*/
-			Boolean state = true;
-			String message = "";
-			FXMLLoader fxmlLoader = new FXMLLoader();
-			Parent scene;
-			try{
-	// Validate that the Exercise type is set. Create and set correct corresponding exercise model.
-				if(typeField.getValue() == null){ //Type need to be chosen before we can do anything, so we can create correct exercise model.
-					message += "Exercise TYPE, need to be choosen!\n";
-					state = false;
-				}
-// Strength Exercise
-				else if(typeField.getValue().equals("Strength")){ //Do strength exercise validations
-					setStrenghtModel(new Strength());
-					//do all the validations:
-	// Validate Exercise Name 
-					if(exerciseField.getText() == null || exerciseField.getText().equals("")){
-						state = false;
-						message += "Exercise name not filled in.\n";
-					}
-					else {
-						this.strengthExercise.setName(exerciseField.getText());
-					}
-	// Validate Exercise Description
-					if(descriptionField.getText().equals("")){
-						state = false;
-						message += "Exercise description not filled in.\n";
-					}
-					else{
-						this.strengthExercise.setDescription(descriptionField.getText());
-					}
-	// Validate Exercise Goal
-					if(goalField.getText().equals("")){
-						state = false;
-						message += "Exercise goal not filled in.\n";
-					}
-					else{
-						//how do we handle the goal of each exercise? do we need a new class/ entity class Goal?
-					}
-	// Validate group/category
-					if(groupField.getValue()== null){
-						state = false;
-						message += "Exercise group not set.\n";
-					}
-					else{
-						//how do we handle this relation to other exercises of same group? should it be only the exercises within same workout with same group that should be grouped?
-					}
-	// Validate type specific 
-					if(Workout.getWorkoutType().equals("Inside")){
-						if(specOneField.getValue() == null){
-							state = false;
-							message += "Exercise Air ventilation not set.\n";
-						}
-						else{
-							this.strengthExercise.setVentilation(specOneField.getValue());
-						}
-						if(specTwoField.getValue()== null){
-							state = false;
-							message += "Exercise spectators not set.\n";
-						}
-						else{
-							this.strengthExercise.setSpectators(Integer.valueOf(specTwoField.getValue()));
-						}
-					}
-					else { // type = Outside
-						if(specOneField.getValue() == null){
-							state = false;
-							message += "Exercise temperature not set.\n";
-						}
-						else{
-							this.strengthExercise.setTemp(Integer.valueOf(specOneField.getValue()));
-						}
-						if(specTwoField.getValue()== null){
-							state = false;
-							message += "Exercise weather not set.\n";
-						}
-						else{
-							this.strengthExercise.setWeather(specTwoField.getValue());
-						}
-					}
-	// Validate reps
-					if(repsField.getText().equals("")){
-						state = false;
-						message += "Exercise repetitions not set.\n";
-					}
-					else{
-						//how do we handle this relation to other exercises of same group? should it be only the exercises within same workout with same group that should be grouped?
-					}
-	// Validate sets
-					if(setsField.getText().equals("")){
-						state = false;
-						message += "Exercise sets not set.\n";	
-					}
-					else {
-						//how do we handle this relation to other exercises of same group? should it be only the exercises within same workout with same group that should be grouped?
-					}
-	// Validate Weight
-					if(weightField.getText().equals("")){
-						state = false;
-						message += "Exercise weigth not set.\n";
-					}
-					else{
-						//how do we handle this relation to other exercises of same group? should it be only the exercises within same workout with same group that should be grouped?
-					}
-	// Validate Length
-					//No need to validate or set, because Strength exercises do not need length.
 
+	}
+
+	//Scene handler: All validation, model update and view update must happend here. First thing we must do, is set the model based on exercise type!!
+	@FXML
+	private void addBtnClicked(){
+		/* Exercise attributes that cannot be Null:
+		 * ØvelsesID - int - auto_increment - PK- Unique.
+		 * Navn - varchar(45)
+		 * Beskrivelse - varchar(45)
+		 * 
+		 * Strength-tabell:
+		 * ØvelsesID - int
+		 * Belastning - int
+		 * Antall repetisjoner - int
+		 * Antall sett - int
+		 * 
+		 * Utholdenhet-tabell
+		 * ØvelsesID - int
+		 * Lengde km - int
+		 * Minutter - int
+		 */
+		Boolean state = true;
+		String message = "";
+		FXMLLoader fxmlLoader = new FXMLLoader();
+		Parent scene;
+		try{
+			// Validate that the Exercise type is set. Create and set correct corresponding exercise model.
+			if(typeField.getValue() == null){ //Type need to be chosen before we can do anything, so we can create correct exercise model.
+				message += "Exercise TYPE, need to be choosen!\n";
+				state = false;
+			}
+			// Strength Exercise
+			else if(typeField.getValue().equals("Strength")){ //Do strength exercise validations
+				setStrenghtModel(new Strength());
+				//do all the validations:
+				// Validate Exercise Name 
+				if(exerciseField.getText() == null || exerciseField.getText().equals("")){
+					state = false;
+					message += "Exercise name not filled in.\n";
 				}
-// Endurance Exercise
-				else if(typeField.getValue().equals("Endurance")){ //Do endurance exercise validations
-					setEnduranceModel(new Endurance());
-					//do all the validations:
-	// Validate Exercise name
-					if(exerciseField.getText() == null || exerciseField.getText().equals("")){
+				else {
+					this.strengthExercise.setName(exerciseField.getText());
+				}
+				// Validate Exercise Description
+				if(descriptionField.getText().equals("")){
+					state = false;
+					message += "Exercise description not filled in.\n";
+				}
+				else{
+					this.strengthExercise.setDescription(descriptionField.getText());
+				}
+				// Validate Exercise Goal
+				if(goalField.getText().equals("")){
+					state = false;
+					message += "Exercise goal not filled in.\n";
+				}
+				else{
+					//how do we handle the goal of each exercise? do we need a new class/ entity class Goal?
+				}
+				// Validate group/category
+				if(groupField.getValue()== null){
+					state = false;
+					message += "Exercise group not set.\n";
+				}
+				else{
+					//how do we handle this relation to other exercises of same group? should it be only the exercises within same workout with same group that should be grouped?
+				}
+				// Validate type specific 
+				if(Workout.getWorkoutType().equals("Inside")){
+					if(specOneField.getValue() == null){
 						state = false;
-						message += "Exercise name not filled in.\n";
-					}
-					else {
-						this.enduranceExercise.setName(exerciseField.getText());
-					}
-	// Validate Exercise Description
-					if(descriptionField.getText().equals("")){
-						state = false;
-						message += "Exercise description not filled in.\n";
+						message += "Exercise Air ventilation not set.\n";
 					}
 					else{
-						this.enduranceExercise.setDescription(descriptionField.getText());
+						this.strengthExercise.setVentilation(specOneField.getValue());
 					}
-	// Validate Exercise Goal
-					if(goalField.getText().equals("")){
+					if(specTwoField.getValue()== null){
 						state = false;
-						message += "Exercise goal not filled in.\n";
+						message += "Exercise spectators not set.\n";
 					}
 					else{
-						//how do we handle the goal of each exercise? do we need a new class/ entity class Goal?
+						this.strengthExercise.setSpectators(Integer.valueOf(specTwoField.getValue()));
 					}
-	// Validate group/category
-					if(groupField.getValue()== null){
+				}
+				else { // type = Outside
+					if(specOneField.getValue() == null){
 						state = false;
-						message += "Exercise group not set.\n";
+						message += "Exercise temperature not set.\n";
 					}
 					else{
-						//how do we handle this relation to other exercises of same group? should it be only the exercises within same workout with same group that should be grouped?
+						this.strengthExercise.setTemp(Integer.valueOf(specOneField.getValue()));
 					}
-	// Validate type specific 
-					if(Workout.getWorkoutType().equals("Inside")){
-						if(specOneField.getValue() == null){
-							state = false;
-						}
-						else{
-						}
-						if(specTwoField.getValue()== null){
-							state = false;
-						}
-						else{
-						}
+					if(specTwoField.getValue()== null){
+						state = false;
+						message += "Exercise weather not set.\n";
 					}
-					else { // type = Outside
-						if(specOneField.getValue() == null){
-							state = false;
-						}
-						else{
-						}
-						if(specTwoField.getValue()== null){
-							state = false;
-						}
-						else{
-						}
+					else{
+						this.strengthExercise.setWeather(specTwoField.getValue());
 					}
-	// Validate reps
+				}
+				// Validate reps
+				if(repsField.getText().equals("")){
+					state = false;
+					message += "Exercise repetitions not set.\n";
+				}
+				else{
+					//how do we handle this relation to other exercises of same group? should it be only the exercises within same workout with same group that should be grouped?
+				}
+				// Validate sets
+				if(setsField.getText().equals("")){
+					state = false;
+					message += "Exercise sets not set.\n";	
+				}
+				else {
+					//how do we handle this relation to other exercises of same group? should it be only the exercises within same workout with same group that should be grouped?
+				}
+				// Validate Weight
+				if(weightField.getText().equals("")){
+					state = false;
+					message += "Exercise weigth not set.\n";
+				}
+				else{
+					//how do we handle this relation to other exercises of same group? should it be only the exercises within same workout with same group that should be grouped?
+				}
+				// Validate Length
+				//No need to validate or set, because Strength exercises do not need length.
+
+			}
+			// Endurance Exercise
+			else if(typeField.getValue().equals("Endurance")){ //Do endurance exercise validations
+				setEnduranceModel(new Endurance());
+				//do all the validations:
+				// Validate Exercise name
+				if(exerciseField.getText() == null || exerciseField.getText().equals("")){
+					state = false;
+					message += "Exercise name not filled in.\n";
+				}
+				else {
+					this.enduranceExercise.setName(exerciseField.getText());
+				}
+				// Validate Exercise Description
+				if(descriptionField.getText().equals("")){
+					state = false;
+					message += "Exercise description not filled in.\n";
+				}
+				else{
+					this.enduranceExercise.setDescription(descriptionField.getText());
+				}
+				// Validate Exercise Goal
+				if(goalField.getText().equals("")){
+					state = false;
+					message += "Exercise goal not filled in.\n";
+				}
+				else{
+					//how do we handle the goal of each exercise? do we need a new class/ entity class Goal?
+				}
+				// Validate group/category
+				if(groupField.getValue()== null){
+					state = false;
+					message += "Exercise group not set.\n";
+				}
+				else{
+					//how do we handle this relation to other exercises of same group? should it be only the exercises within same workout with same group that should be grouped?
+				}
+				// Validate type specific 
+				if(Workout.getWorkoutType().equals("Inside")){
+					if(specOneField.getValue() == null){
+						state = false;
+						message += "Exercise Air ventilation not set.\n";
+					}
+					else{
+						this.strengthExercise.setVentilation(specOneField.getValue());
+					}
+					if(specTwoField.getValue()== null){
+						state = false;
+						message += "Exercise spectators not set.\n";
+					}
+					else{
+						this.strengthExercise.setSpectators(Integer.valueOf(specTwoField.getValue()));
+					}
+				}
+				else { // type = Outside
+					if(specOneField.getValue() == null){
+						state = false;
+						message += "Exercise temperature not set.\n";
+					}
+					else{
+						this.strengthExercise.setTemp(Integer.valueOf(specOneField.getValue()));
+					}
+					if(specTwoField.getValue()== null){
+						state = false;
+						message += "Exercise weather not set.\n";
+					}
+					else{
+						this.strengthExercise.setWeather(specTwoField.getValue());
+					}
+					// Validate reps
 					if(repsField.getText().equals("")){
 						state = false;
 						message += "Exercise repetitions not set.\n";
@@ -313,7 +320,7 @@ public class ExcercisesController  implements PropertyChangeListener {
 					else{
 						//how do we handle this relation to other exercises of same group? should it be only the exercises within same workout with same group that should be grouped?
 					}
-	// Validate sets
+					// Validate sets
 					if(setsField.getText().equals("")){
 						state = false;
 						message += "Exercise sets not set.\n";	
@@ -321,7 +328,7 @@ public class ExcercisesController  implements PropertyChangeListener {
 					else {
 						//how do we handle this relation to other exercises of same group? should it be only the exercises within same workout with same group that should be grouped?
 					}
-	// Validate Weight
+					// Validate Weight
 					if(weightField.getText().equals("")){
 						state = false;
 						message += "Exercise weigth not set.\n";
@@ -329,18 +336,16 @@ public class ExcercisesController  implements PropertyChangeListener {
 					else{
 						//how do we handle this relation to other exercises of same group? should it be only the exercises within same workout with same group that should be grouped?
 					}
-	// Validate Length
+					// Validate Length
 					if(lenghtField.getText().equals("")){
 						state = false;
-						message += "Exercise length not st.\n";
+						message += "Exercise length not set.\n";
 					}
 					else{
 						this.enduranceExercise.setLength(Integer.valueOf(lenghtField.getText().split(" ")[0]));
 					}
-					
-
 				}
-// Create State validation
+				// Create State validation
 				if(state){ //The exercise to add is valid. do something 
 					scene = (Parent) fxmlLoader.load(this.getClass().getResourceAsStream("DiaryGUI.fxml"));
 					Main.primaryStage.setScene(new Scene(scene));
@@ -349,8 +354,9 @@ public class ExcercisesController  implements PropertyChangeListener {
 					Alertbox.display(message);
 				}
 			}
-			catch(Exception e){
-				e.printStackTrace();
-			}
 		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	}
 }
