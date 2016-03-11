5799af54987320739a9d414453a0bfc5d883bdcc
Martin Stigen <martin.stigen@live.no>
2016-03-11 13:59:31 +0100

    Skeleton finished

--- TDT4145/src/application/Tabell.java
+++ TDT4145/src/application/Tabell.java
@@ -8,18 +8,69 @@ package application;
 
 public class Tabell {
 	
-	public static class Insert {
+	public static class INSERT {
 		
 		private static final String TODO = "INSERT INTO ";
 		
-		public static String Innendørsøvelse(int øvelsesID, String luftventilasjon, int antallTilskuere) {
-			return TODO + "Innendørsøvelse VALUES(" + øvelsesID + ", '" + luftventilasjon + "', " + antallTilskuere +");";
+		public static String INNENDØRSØVELSE(int øvelsesID, String luftventilasjon, int antallTilskuere) {
+			return TODO + "Innendørsøvelse VALUES(" + øvelsesID + ", " + valueOf(luftventilasjon) + ", " + antallTilskuere +");";
+		}
+		
+		public static String MÅL(int målNr, String tidsperiode, String beskrivelse, int øvelsesID) {
+			return TODO + values(valueOf(målNr), valueOf(tidsperiode), valueOf(beskrivelse), valueOf(øvelsesID));
+		}
+		
+		public static String RESULTAT(int resultatNr, String besteResultat, String dato, int øvelsesID) {
+			return TODO;
+		}
+		
+		public static String STYRKE_OG_KONDISJON(int øvelsesID, int belastning, int antallRep, int antallSett) {
+			return TODO;
+		}
+		
+		public static String TRENINGSØKT(int treningsID, String dato, String tidspunkt, int varighet, int personligForm, int prestasjon, String notat) {
+			return TODO;
+		}
+		
+		public static String UTENDØRSØVELSE(int øvelsesID, int temperatur, String værtype) {
+			return TODO;
+		}
+		
+		public static String UTHOLDENHET(int øvelsesID, int lengdeKm, int minutter) {
+			return TODO;
+		}
+		
+		public static String ØVELSE(int øvelsesID, String navn, String beskrivelse) {
+			return TODO;
 		}
 	}
 	
+	private static String valueOf(String value) {
+		return "'" + value + "'";
+	}
 	
+	private static String valueOf(int value) {
+		return String.valueOf(value);
+	}
+	
+	private static String values(String... values) {
+		StringBuilder statement = new StringBuilder();
+		
+		for (String value : values) {
+			if (statement.length() == 0) {
+				statement.append("VALUES(");
+			}
+			else {
+				statement.append(", ");
+			}
+			statement.append(value);
+		}
+		
+		statement.append(");");
+		return statement.toString();
+	}
 	
 	public static void main(String[] args) {
-		System.out.println(Tabell.Insert.Innendørsøvelse(1, "bra", 10));
+		System.out.println(Tabell.INSERT.MÅL(5, "Hei", "Anal", 55));
 	}
 }
